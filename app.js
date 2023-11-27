e04e41d16380fb115a148a9cf8920e24b3368e4d
MuhammadSaddique <saddiqueshah588@gmail.com>
2023-11-28 02:05:09 +0500

    tweet

--- app.js
+++ app.js
@@ -1032,6 +1032,36 @@ var btn = document.querySelector("#v2");
 
 /////////////////////////// 262 
 
+// var tweetForm = document.querySelector("#tweetform");
+// var tweetContainer = document.querySelector('#tweets');
+// tweetForm.addEventListener("submit",function(e){
+//     e.preventDefault();
+
+//     // var usernameInput = document.querySelectorAll("input")[0];
+//     // var tweetInput = document.querySelectorAll("input")[1];
+   
+//     var usernameInput = tweetForm.elements.username;
+//     var tweetInput = tweetForm.elements.tweet;
+
+//     addTweet(usernameInput.value, tweetInput.value);
+//     usernameInput.value = '';
+//     tweetInput.value = '';
+    
+
+// });
+
+// var addTweet = function(username,tweet){
+//     var newtweet = document.createElement('li');
+//     var bTag = document.createElement('b');
+
+//     bTag.append(username);
+//     newtweet.append(bTag);
+
+//     newtweet.append(`- ${tweet}`);
+//     tweetContainer.append(newtweet);
+// }
+
+
 
 
 
