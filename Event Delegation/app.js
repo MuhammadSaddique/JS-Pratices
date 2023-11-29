2917ca820025ac4231f6b23d29d75498c9abd4fc
MuhammadSaddique <saddiqueshah588@gmail.com>
2023-11-29 23:44:58 +0500

    Events Delegation

---
+++ Event Delegation/app.js
@@ -0,0 +1,30 @@
+var tweetForm = document.querySelector('#tweetForm');
+var tweetContainer = document.querySelector('#tweets');
+
+
+tweetForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    
+    var usernameInput = tweetForm.elements.username;
+    var tweetInput = tweetForm.elements.tweet;
+
+    addTweet(usernameInput.value, tweetInput.value);
+    usernameInput.value = '';
+    tweetInput.value = '';
+    
+
+})
+
+function addTweet(){
+
+    var newTweet = document.createElement('li');
+    var bTag = document.createElement('b');
+    
+    bTag.append(username);
+    newTweet.append(bTag);
+    
+    newTweet.append(`- ${tweet}`);
+    tweetContainer.append(newTweet);
+}
+
+
